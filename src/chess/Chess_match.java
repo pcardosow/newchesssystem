72da7b232c850718139863342b1a0097f7317492
Pedro Cardoso <pcardosow.nnf@gmail.com>
2020-07-22 20:09:09 -0300

    King and tower added to the board

--- src/chess/Chess_match.java
+++ src/chess/Chess_match.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class Chess_match {
 	private Board board;
 
 	public Chess_match() {
 		board = new Board(8, 8);
+		InitialSetup();
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -17,5 +21,14 @@ public class Chess_match {
 			}
 		}
 		return mat;
+	}
+	private void InitialSetup() {
+		board.placePiece(new Rook(board, Color.BLACK), new Position(0,0));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(0,7));
+		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+		
+		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(7,0));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(7,7));
 	}
 }
